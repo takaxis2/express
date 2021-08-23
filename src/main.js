382dbee34f2991ecc898834d5a203877cf0e4741
takaxis2 <axis1130@gmail.com>
2021-08-23 21:24:54 +0900

    'middleware'

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 //@ts-check
 
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 
 const PORT = 5000;
 
-app.use("/", (req, res) => {
-  res.send(`hello, express`);
+app.use("/", async (req, res, next) => {
+  console.log("Middleware 1");
+
+  const fileContent = await fs.readFile(".gitignore", "utf-8");
+
+  const reqAt = new Date();
+  //@ts-ignore
+  req.reqAt = reqAt;
+  //@ts-ignore
+  req.fileContent = fileContent;
+  next();
+});
+
+app.use((req, res) => {
+  console.log("MiddleWare 2");
+  //@ts-ignore
+  res.send(`hello, express: ${req.reqAt} ${req.fileContent}`);
 });
 
 app.listen(PORT, () => {
